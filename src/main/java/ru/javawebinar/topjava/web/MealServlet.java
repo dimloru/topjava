442983ba82c616e6d2529eed9e382da039372456
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-07-09 14:57:38 +0300

    added appComtext impl into mealServlet, spring working

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.repository.mock.InMemoryMealRepositoryImpl;
 import ru.javawebinar.topjava.service.MealService;
@@ -19,18 +21,21 @@ import java.util.Objects;
 public class MealServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
 
-    private MealRepository repository;
-    private MealService mealService;
-    private MealRestController mealRestController;
+//    private MealRepository repository;
+//    private MealService mealService;
+//    private MealRestController mealRestController;
+
+    private ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+    private MealRestController mealRestController = context.getBean(MealRestController.class);
 
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        repository = new InMemoryMealRepositoryImpl();
-        mealService = new MealServiceImpl(repository);
-        mealRestController = new MealRestController(mealService);
-    }
+//    @Override
+//    public void init(ServletConfig config) throws ServletException {
+//        super.init(config);
+//        repository = new InMemoryMealRepositoryImpl();
+//        mealService = new MealServiceImpl(repository);
+//        mealRestController = new MealRestController(mealService);
+//    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
