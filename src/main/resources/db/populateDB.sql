c1f519021fc7baef555e0babe0c21b60d454848b
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-07-17 13:00:07 +0300

    SQL scripts (init, populate) for meals db done

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,7 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +11,15 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+
+  (100000, TO_TIMESTAMP('2015-05-30 10:00', 'YYYY-MM-DD HH24:MI'), 'Завтрак', 500),
+  (100000, TO_TIMESTAMP('2015-05-30 13:00', 'YYYY-MM-DD HH24:MI'), 'Обед', 1000),
+  (100000, TO_TIMESTAMP('2015-05-30 20:00', 'YYYY-MM-DD HH24:MI'), 'Ужин', 500),
+  (100000, TO_TIMESTAMP('2015-05-31 10:00', 'YYYY-MM-DD HH24:MI'), 'Завтрак', 1000),
+  (100000, TO_TIMESTAMP('2015-05-31 13:00', 'YYYY-MM-DD HH24:MI'), 'Обед', 500),
+  (100000, TO_TIMESTAMP('2015-05-31 20:00', 'YYYY-MM-DD HH24:MI'), 'Ужин', 510),
+
+  (100001, TO_TIMESTAMP('2015-06-01 14:00', 'YYYY-MM-DD HH24:MI'), 'Админ ланч', 510),
+  (100001, TO_TIMESTAMP('2015-06-01 21:00', 'YYYY-MM-DD HH24:MI'), 'Админ ужин', 1500);
