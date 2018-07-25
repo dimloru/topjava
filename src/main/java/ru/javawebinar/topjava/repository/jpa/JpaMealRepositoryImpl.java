4024eb3813c7073da086fdb0395c43dcc06846f0
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-07-25 16:34:32 +0300

    Most of Criteria done (w/except for getAllBetween)

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -10,8 +10,10 @@ import ru.javawebinar.topjava.util.ValidationUtil;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Parameter;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
@@ -26,17 +28,17 @@ public class JpaMealRepositoryImpl implements MealRepository {
     @Override
     @Transactional
     public Meal save(Meal meal, int userId) {
+        User ref = em.getReference(User.class, userId);
 
+        // Dont't see the point in using criteria here
 
         if (meal.isNew()) {
-            meal.setUser(em.getReference(User.class, userId));
+            meal.setUser(ref);
             em.persist(meal);
             return meal;
         }
 
         ValidationUtil.checkNotFound(get(meal.getId(), userId), "While updating");
-
-        User ref = em.getReference(User.class, userId);
         meal.setUser(ref);
 
         return em.merge(meal);
@@ -47,15 +49,41 @@ public class JpaMealRepositoryImpl implements MealRepository {
     public boolean delete(int id, int userId) {
         User ref = em.getReference(User.class, userId);
 
-        return em.createQuery("DELETE FROM Meal m WHERE m.id=:id AND m.user=:user")
-                .setParameter("id", id)
-                .setParameter("user", ref)
-                .executeUpdate() != 0;
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaDelete<Meal> q = cb.createCriteriaDelete(Meal.class);
+        Root m = q.from(Meal.class);
+        q.where(
+                cb.equal(m.get("id"), id),
+                cb.equal(m.get("user"), ref));
+        return em.createQuery(q).executeUpdate() != 0;
+
+
+//        return em.createQuery("DELETE FROM Meal m WHERE m.id=:id AND m.user=:user")
+//                .setParameter("id", id)
+//                .setParameter("user", ref)
+//                .executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
         User ref = em.getReference(User.class, userId);
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Meal> q = cb.createQuery(Meal.class);
+        Root<Meal> m = q.from(Meal.class);
+        ParameterExpression<Integer> param = cb.parameter(Integer.class);
+        q.select(m).where(
+                cb.equal(m.get("id"), param),
+                cb.equal(m.get("user"), ref));
+
+
+        TypedQuery<Meal> query = em.createQuery(q);
+        query.setParameter(param, id);
+
+        List<Meal> meals = query.getResultList();
+
+        /*
+        // Old JPQL version
 
         // Changed Query to TypedQuery
         TypedQuery<Meal> q = em.createQuery("SELECT m FROM Meal m WHERE m.id=:id AND m.user=:user", Meal.class);
@@ -64,6 +92,7 @@ public class JpaMealRepositoryImpl implements MealRepository {
                 .setParameter("id", id)
                 .setParameter("user", ref)
                 .getResultList();
+        */
 
         return  DataAccessUtils.singleResult(meals);
     }
@@ -72,9 +101,20 @@ public class JpaMealRepositoryImpl implements MealRepository {
     public List<Meal> getAll(int userId) {
         User ref = em.getReference(User.class, userId);
 
-        return em.createNamedQuery(Meal.GET_All, Meal.class)
-                .setParameter("user", ref)
-                .getResultList();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Meal> q = cb.createQuery(Meal.class);
+        Root<Meal> m = q.from(Meal.class);
+        q.select(m);
+        q.where(cb.equal(m.get("user"), ref));
+        q.orderBy(cb.desc(m.get("dateTime")));
+
+        TypedQuery<Meal> query = em.createQuery(q);
+        return query.getResultList();
+
+
+//        return em.createNamedQuery(Meal.GET_All, Meal.class)
+//                .setParameter("user", ref)
+//                .getResultList();
     }
 
     @Override
